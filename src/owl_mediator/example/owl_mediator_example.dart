8a4ec02bdedbd2d97f9f8f9dfd4926d238edd58b
Nightowl286 <nightowl286@protonmail.com>
2026-02-09 19:38:13 +0000

    Add default dart package things.

---
+++ src/owl_mediator/example/owl_mediator_example.dart
@@ -0,0 +1,6 @@
+import 'package:owl_mediator/owl_mediator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ src/owl_mediator/test/owl_mediator_test.dart
@@ -0,0 +1,16 @@
+import 'package:owl_mediator/owl_mediator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
