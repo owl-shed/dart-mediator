698be6dcfbcf14f5e8f96c6ccbeaf8d9135fff6d
Nightowl286 <nightowl286@protonmail.com>
2026-02-10 21:03:28 +0000

    Add missing documentation for the request interfaces.

--- src/lib/src/request.dart
+++ src/lib/src/request.dart
@@ -1,5 +1,11 @@
+/// Represents the base unit type for commands and queries, you shouldn't implement this interface directly.
+///
+/// You can however use it if you want to generically work with both commands and queries.
 abstract interface class IRequest<TResult> {}
 
+/// Represents the base unit type for command and query handlers, you shouldn't implement this interface directly.
+///
+/// You can however use it if you want to generically work with both command and query handlers.
 abstract interface class IRequestHandler<
   TRequest extends IRequest<TResult>,
   TResult
